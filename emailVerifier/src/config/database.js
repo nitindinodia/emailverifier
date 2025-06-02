385d0d47255b0183b35a9228cc5e1dd394812f39
“Nitin <nitindinodia@gmail.com>
2025-06-02 08:48:07 +0530

    security enhanced - created variable for mongodb password in env file

--- emailVerifier/src/config/database.js
+++ emailVerifier/src/config/database.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
+const MONGO_PASSWORD = require("../../.env")
+
 const connectDb = async ()=>{
-    await mongoose.connect("mongodb+srv://nitindinodia:Zxo9YlyWHSS517V0@namastenode.taxlf5g.mongodb.net/emailVerifier");
+    await mongoose.connect(`mongodb+srv://nitindinodia:${MONGO_PASSWORD}@namastenode.taxlf5g.mongodb.net/emailVerifier`);
 }
 
 module.exports = connectDb;
